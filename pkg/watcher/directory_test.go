8faaf718765e29f697f8ab70d10235ad016ae76a
Adolfo García Veytia (Puerco) <puerco@chainguard.dev>
2022-07-09 19:39:40 -0500

    Watcher: Add interface and Directory impl
    This commit adds the watcher interface to detect changes caused by
    processes and record them.
    
    In addition to the first interface, this commit adds the first
    implenmentation: A directory watcher.
    
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@chainguard.dev>

---
+++ pkg/watcher/directory_test.go
@@ -0,0 +1,130 @@
+/*
+Copyright 2022 Adolfo García Veytia
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package watcher
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestDirectorySnap(t *testing.T) {
+	// Create a fixed time to make the times deterministic
+	fixedTime := time.Date(1976, time.Month(2), 10, 23, 30, 30, 0, time.Local)
+
+	// Create some files in the directory
+	for _, tc := range []struct {
+		prepare func(path string) error
+		mutate  func(path string) error
+		expect  []File
+	}{
+		// Two emtpy directories. No error, no change
+		{
+			func(path string) error { return nil },
+			func(path string) error { return nil },
+			[]File{},
+		},
+		// One file, unchanged at mutation time
+		{
+			func(path string) error {
+				return os.WriteFile(filepath.Join(path, "test.txt"), []byte("test"), os.FileMode(0o644))
+			},
+			func(path string) error { return nil },
+			[]File{},
+		},
+		// One file, rewritten should be reported
+		{
+			func(path string) error {
+				return os.WriteFile(filepath.Join(path, "test.txt"), []byte("test"), os.FileMode(0o644))
+			},
+			func(path string) error {
+				filePath := filepath.Join(path, "test.txt")
+				if err := os.WriteFile(
+					filePath, []byte("test"), os.FileMode(0o644),
+				); err != nil {
+					return err
+				}
+				if err := os.Chtimes(filePath, fixedTime, fixedTime); err != nil {
+					return err
+				}
+				return nil
+			},
+			[]File{
+				{
+					Path: "test.txt",
+					Time: fixedTime,
+					Hash: "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08",
+				}},
+		},
+		// One file, with contents changed
+		{
+			func(path string) error {
+				filePath := filepath.Join(path, "test.txt")
+				if err := os.WriteFile(
+					filePath, []byte("test"), os.FileMode(0o644),
+				); err != nil {
+					return err
+				}
+				if err := os.Chtimes(filePath, fixedTime, fixedTime); err != nil {
+					return err
+				}
+				return nil
+			},
+			func(path string) error {
+				filePath := filepath.Join(path, "test.txt")
+				if err := os.WriteFile(
+					filePath, []byte("test, but with a change!"), os.FileMode(0o644),
+				); err != nil {
+					return err
+				}
+				if err := os.Chtimes(filePath, fixedTime, fixedTime); err != nil {
+					return err
+				}
+				return nil
+			},
+			[]File{
+				{
+					Path: "test.txt",
+					Time: fixedTime,
+					Hash: "76aad9c1d52e424d0dd6c6b8e07169d5d5f9001a06fe5343d4bfa13c804788f0",
+				}},
+		},
+	} {
+		// Create a temp directory to operate in
+		dir, err := os.MkdirTemp("", "")
+		require.NoError(t, err)
+		defer os.RemoveAll(dir)
+
+		// Create the directory watcher
+		sut := Directory{
+			Path:      dir,
+			Snapshots: []Snapshot{},
+		}
+
+		require.NoError(t, tc.prepare(dir))
+		require.NoError(t, sut.Snap(), "creating first snapshot")
+
+		require.NoError(t, tc.mutate(dir))
+		require.NoError(t, sut.Snap(), "creating mutated fs snapshot")
+
+		delta := sut.Snapshots[0].Delta(&sut.Snapshots[1])
+		require.Equal(t, delta, tc.expect)
+	}
+}
---
+++ pkg/watcher/watcher_test.go
@@ -0,0 +1,74 @@
+/*
+Copyright 2022 Adolfo García Veytia
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package watcher
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestDelta(t *testing.T) {
+	testFile := File{
+		"test.txt", "c71d239df91726fc519c6eb72d318ec65820627232b2f796219e87dcf35d0ab4", time.Now(),
+	}
+	modHashFile := File{
+		"test.txt", "25b89320221dda5abe3df4624d246d22d0c820ee3598e97553611d7c80abbd36", testFile.Time,
+	}
+	modTimeFile := File{
+		"test.txt", "25b89320221dda5abe3df4624d246d22d0c820ee3598e97553611d7c80abbd36",
+		time.Date(1976, time.Month(2), 10, 23, 30, 30, 0, time.Local),
+	}
+	for _, tc := range []struct {
+		preSnap  Snapshot
+		postSnap Snapshot
+		expect   []File
+	}{
+		{
+			// Empty snapshots, should be an empty list
+			Snapshot{},
+			Snapshot{},
+			[]File{},
+		},
+		{
+			// One removed file, should be empty list
+			Snapshot{testFile.Path: testFile},
+			Snapshot{},
+			[]File{},
+		},
+		{
+			// One added file, should be a list with that file
+			Snapshot{},
+			Snapshot{testFile.Path: testFile},
+			[]File{testFile},
+		},
+		{
+			// One file with time modded, should be a list with that file
+			Snapshot{testFile.Path: testFile},
+			Snapshot{testFile.Path: modTimeFile},
+			[]File{modTimeFile},
+		},
+		{
+			// One file with hash modded, should be a list with that file
+			Snapshot{testFile.Path: testFile},
+			Snapshot{testFile.Path: modHashFile},
+			[]File{modHashFile},
+		},
+	} {
+		require.Equal(t, tc.expect, tc.preSnap.Delta(&tc.postSnap))
+	}
+}
