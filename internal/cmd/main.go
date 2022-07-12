726f0938cbd91edade6a30459e621db2a42bb5b3
Adolfo García Veytia (Puerco) <puerco@chainguard.dev>
2022-07-12 00:07:20 -0500

    Add initial tejolote CLI and cobra commands
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@chainguard.dev>

---
+++ internal/cmd/main.go
@@ -0,0 +1,80 @@
+/*
+Copyright 2022 Adolfo García Veytia
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"sigs.k8s.io/release-utils/log"
+)
+
+func Execute() error {
+	var rootCmd = &cobra.Command{
+		Short: "A tool for working with SPDX manifests",
+		Long: `tejolote (the handle of a molcajete, where you make salsa)
+	
+	🌶 tejolote is a utility that allows a developer to execute a 
+	process - ideally a builder - and record its inputs and outputs.
+	The main goal is to obtain provenance information of builds
+	and other transformations when building and shipping software.
+	
+	In its simplest form, you can precede your existing build 
+	command with tejolote run and it will make its best to create a
+	meaningful attestation. For example:
+	
+		Change this:
+		make build
+	
+		With this:
+		tejolote run make all
+	
+	Tejolote will try to make sane asumptions but for best results, it
+	allows for full control of the process you run.
+	
+	`,
+		Use:               "tejolote",
+		SilenceUsage:      false,
+		PersistentPreRunE: initLogging,
+	}
+
+	rootCmd.PersistentFlags().StringVar(
+		&commandLineOpts.logLevel,
+		"log-level",
+		"info",
+		fmt.Sprintf("the logging verbosity, either %s", log.LevelNames()),
+	)
+
+	addRun(rootCmd)
+
+	if err := rootCmd.Execute(); err != nil {
+		logrus.Fatal(err)
+		return err
+	}
+	return nil
+}
+
+type commandLineOptions struct {
+	logLevel string
+}
+
+var commandLineOpts = &commandLineOptions{}
+
+func initLogging(*cobra.Command, []string) error {
+	return log.SetupGlobalLogger(commandLineOpts.logLevel)
+}
---
+++ internal/cmd/run.go
@@ -0,0 +1,138 @@
+/*
+Copyright 2022 Adolfo García Veytia
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	gexec "os/exec"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+
+	"github.com/puerco/tejolote/pkg/config"
+	"github.com/puerco/tejolote/pkg/exec"
+	"github.com/puerco/tejolote/pkg/watcher"
+)
+
+type runOptions struct {
+	Verbose    bool
+	OutputDirs []string
+}
+
+func addRun(parentCmd *cobra.Command) {
+	runOpts := runOptions{}
+	runCmd := &cobra.Command{
+		Short: "Execute one or more builder steps",
+		Long: `tejolote run [command]
+	
+The run subcommand os tejolote executes a process intended to
+transform files. Generally this happens as part of a build, patching
+or cloning repositories.
+
+Tejolote will monitor for changes that ocurred during the command
+execution and will attest to them to generate provenance data of
+where they came from.
+	
+	`,
+		Use:               "run",
+		SilenceUsage:      false,
+		PersistentPreRunE: initLogging,
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			var runner *exec.Runner
+			runner, err = buildRunner(runOpts)
+			if err != nil {
+				return fmt.Errorf("creating runner: %w", err)
+			}
+
+			var step *config.Step
+			if len(args) > 0 {
+				step, err = syntheticStepFromArgs(args...)
+				if err != nil {
+					return fmt.Errorf("generating step from arguments: %w", err)
+				}
+			}
+
+			if step == nil {
+				logrus.Warn("💣 Error. Nothing to execute.")
+				logrus.Warn("Define something to run in the command line or define one or more steps")
+				logrus.Warn("in a configuration file.")
+
+				return errors.New("no step to run")
+			}
+
+			// What do we do with the run?
+			run, err2 := runner.RunStep(step)
+			if err2 != nil {
+				return fmt.Errorf("executing step: %w", err)
+			}
+
+			logrus.Infof("Run produced %d artifacts", len(run.Artifacts))
+			return nil
+		},
+	}
+
+	runCmd.PersistentFlags().StringSliceVar(
+		&runOpts.OutputDirs,
+		"dir",
+		[]string{"."},
+		"list of directories that tejolote will monitor for output",
+	)
+
+	runCmd.PersistentFlags().BoolVar(
+		&runOpts.Verbose,
+		"verbose",
+		false,
+		"verbose output (prints commands and output)",
+	)
+
+	parentCmd.AddCommand(runCmd)
+}
+
+// buildRunner returns a configured runner
+func buildRunner(opts runOptions) (*exec.Runner, error) {
+	runner := exec.NewRunner()
+
+	for _, dir := range opts.OutputDirs {
+		logrus.Infof("Watching directory: %s", dir)
+		runner.Watchers = append(runner.Watchers, watcher.NewDirectory(dir))
+	}
+
+	return runner, nil
+}
+
+// syntheticStepFromArgs evaluates the arguments passed to see if
+// they correspond to an executable which may be contrued into
+// a tejolote run
+func syntheticStepFromArgs(args ...string) (*config.Step, error) {
+	if len(args) == 0 {
+		return nil, errors.New("no arguments ")
+	}
+
+	// Check for executable
+	if _, err := gexec.LookPath(args[0]); err != nil {
+		return nil, fmt.Errorf("executable '%s' not found", args[0])
+	}
+
+	params := []string{}
+	if len(args) > 1 {
+		params = args[1:]
+	}
+	step := config.NewStep(args[0], params...)
+
+	return &step, nil
+}
