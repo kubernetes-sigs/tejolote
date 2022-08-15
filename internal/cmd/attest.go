f0e51c872cc0482700598ef8f60fd2f77a784317
Adolfo García Veytia (Puerco) <puerco@chainguard.dev>
2022-08-15 13:14:11 -0500

    Make output flags reusable
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@chainguard.dev>

--- internal/cmd/attest.go
+++ internal/cmd/attest.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2022 Adolfo García Veytia
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package cmd
 
 import (
@@ -9,7 +25,7 @@ import (
 )
 
 type attestOptions struct {
-	output    string
+	outputOptions
 	artifacts []string
 }
 
@@ -68,12 +84,7 @@ where they came from.
 		},
 	}
 
-	attestCmd.PersistentFlags().StringVar(
-		&attestOpts.output,
-		"output",
-		"",
-		"output file",
-	)
+	attestOpts.outputOptions = addOutputFlags(attestCmd)
 
 	attestCmd.PersistentFlags().StringSliceVar(
 		&attestOpts.artifacts,
